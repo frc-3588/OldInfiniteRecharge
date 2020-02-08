b37c00309810eea895525766e663c5c7916ee6d1
Computer 3 <lindberghrobotics@gmail.com>
2020-02-08 10:32:53 -0800

    Tai-AcquisitionOffCommand and ShooterOffCommand changed

--- InfiniteRecharge/src/main/java/frc/robot/commands/AcquisitionOffCommand.java
+++ InfiniteRecharge/src/main/java/frc/robot/commands/AcquisitionOffCommand.java
@@ -38,7 +38,7 @@ public class AcquisitionOffCommand extends Command {
     // Called just before this Command runs the first time
     @Override
     protected void initialize() {
-        
+        Robot.acquisition.setAcquisitionPower(0.0);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -49,7 +49,7 @@ public class AcquisitionOffCommand extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
